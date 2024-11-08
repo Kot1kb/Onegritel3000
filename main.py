0740533c71480eee4a780e439fd7b14dde49e75d
kot1kr <cosmodog0901@gmail.com>
2024-11-08 11:51:45 +0200

    ihn

---
+++ main.py
@@ -0,0 +1,12 @@
+from PIL import Image
+
+def Main():
+    print("Main")
+    im = Image.open("wman.jpg")
+    r, g, b = im.split()
+    imn = Image.merge("RGB", (g, b, b))
+    imn.show()
+
+if __name__ == '__main__':
+    print("start")
+    Main()
