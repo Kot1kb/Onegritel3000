38ce53e743acf250e4ad90e0932cf51775da22a2
kot1kr <cosmodog0901@gmail.com>
2024-11-08 12:54:09 +0200

    ihn

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from PIL import Image
+from PIL import ImageFilter
 
 def Main():
     print("Main")
-    im = Image.open("kirillnegrovich.jpg")
+    filepath = input("Input path to file you want to become a black person>>")
+    im = Image.open(filepath)
     r, g, b = im.split()
-    imn = Image.merge("RGB", (g, b, b))
+    ba = b.filter(ImageFilter.MaxFilter)
+    brown_overlay = Image.new("RGB", ba.size, (139, 69, 19))
+
+    image_rgb = ba.convert("RGB")
+
+    brown_image = Image.blend(image_rgb, brown_overlay, alpha=0.1)
+
+    brown_image_l = brown_image.convert("L")
+
+    # Show or save the final image
+    #brown_image_l.show()
+    #b.show()
+    imn = Image.merge("RGB", (g, brown_image_l, brown_image_l))
     imn.show()
+    name = input("Type file name to save for example blackperson.png:")
+    print(name)
+    print(name.find(".png"))
+    try:
+        if name.find(".png") > 0:
+            imn.save(name)
+    except:
+        print("something went wrong")
 
 
 if __name__ == '__main__':
